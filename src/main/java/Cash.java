94c3081042780eec6539807d18a415152bea3495
eriinys <eriinys@duck.com>
2025-10-28 14:05:06 -0500

    changed Assets to abstract class and added Cash subclass

--- src/main/java/Cash.java
+++ src/main/java/Cash.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
-public class Cash {
+public class Cash extends Assets{
+    public double getValue(){
+        return originalCost;
+    }
+    //Class 'Cash' must either be declared abstract or
+    // implement abstract method 'getValue()' in 'Assets'
 }
